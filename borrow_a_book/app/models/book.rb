a86a6c6d0104217292322035ed6d35a9a4d1ce5d
Louisa <louisaspicer@gmail.com>
2017-02-21 21:01:50 +0000

    Added tests for validations

--- borrow_a_book/app/models/book.rb
+++ borrow_a_book/app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
 
+  validates :title, length: { minimum: 2 }, uniqueness: true
+
   has_many :comments, dependent: :destroy
-  
+
 end
--- borrow_a_book/spec/models/book_spec.rb
+++ borrow_a_book/spec/models/book_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 describe Book, type: :model do
 
-  it "is not valid unless it has a unique name" do
+  it "is not valid if title is less than 3 letters" do
+    book = Book.new(title: "A", author: "An author")
+    expect(book).to have(1).error_on(:title)
+  end
+
+  it "is not valid unless title has a unique name" do
     Book.create(title: "A book", author: "An author")
     book = Book.new(title: "A book", author: "An author")
     expect(book).to have(1).error_on(:title)
