da1655bfac10bcca2293d13c3f935e444ed7639f
Louisa <louisaspicer@gmail.com>
2017-03-05 17:38:00 +0000

    Authenticating current user to allow edit and delete

--- borrow_a_book/app/controllers/books_controller.rb
+++ borrow_a_book/app/controllers/books_controller.rb
@@ -26,25 +26,45 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if authenticate_book_owner(@book)
+      @book = Book.find(params[:id])
+    else
+      flash[:alert] = "You can't edit a book that you don't own"
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(books_params)
-    redirect_to('/books')
+    if authenticate_book_owner(@book)
+      @book.update(books_params)
+      redirect_to book_path
+    else
+      flash[:alert] = "You can't edit a book that you don't own"
+      redirect_to books_path
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    flash[:notice] = "#{@book.title} deleted successfully"
-    redirect_to '/books'
+    if authenticate_book_owner(@book)
+      @book.destroy
+      flash[:notice] = "#{@book.title} deleted successfully"
+      redirect_to books_path
+    else
+      flash[:alert] = "You can't delete a book that you don't own"
+      redirect_to books_path
+    end
   end
 
   private
 
   def books_params
     params.require(:book).permit(:title, :author, :image, :user_id)
+  end
+
+  def authenticate_book_owner(book_object)
+     current_user.id == book_object.user_id
   end
 
 end
