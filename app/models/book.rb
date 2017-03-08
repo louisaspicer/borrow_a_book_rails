44eaa6924a1255920d1d5f68e2e9ead0017a2983
Louisa <louisaspicer@gmail.com>
2017-03-08 13:02:07 +0000

    Changed default url for image to a placeholder image

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :requests, dependent: :destroy
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "placeholder.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
